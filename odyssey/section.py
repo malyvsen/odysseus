19c4e1410aaca760fb54afa9b006eb8a99f27ecf
malyvsen <5940672+malyvsen@users.noreply.github.com>
2023-08-06 17:11:59 +0200

    feature: keep section indentation level

--- odyssey/section.py
+++ odyssey/section.py
@@ -5,24 +5,38 @@ from dataclasses import dataclass
 class Section:
     """A piece of a document."""
 
+    level: int
     title: str
     text: str
 
     @classmethod
     def cut_markdown(cls, markdown: str):
         result = []
-        new_title = None
-        new_text = None
+        new_section = None
 
         for line in markdown.split("\n"):
             if line.startswith("#"):
-                if new_title is not None:
-                    result.append(cls(new_title, new_text.strip()))
-                new_title = line.replace("#", "").strip()
-                new_text = ""
-            else:
-                new_text += line
+                if new_section is not None:
+                    result.append(cls.from_markdown(new_section))
+                new_section = ""
+            new_section += line + "\n"
 
-        if new_title is not None:
-            result.append(cls(new_title, new_text.strip()))
+        if new_section is not None:
+            result.append(cls.from_markdown(new_section))
         return result
+
+    @classmethod
+    def from_markdown(cls, markdown: str):
+        if not markdown.startswith("#"):
+            raise ValueError("Section must start with a heading")
+        lines = markdown.split("\n")
+        if any(line.startswith("#") for line in lines[1:]):
+            raise ValueError("Section may contain only one heading")
+        return cls(
+            level=lines[0].count("#"),
+            title=lines[0].replace("#", "").strip(),
+            text="\n".join(line.strip() for line in lines[1:]).strip(),
+        )
+
+    def to_markdown(self):
+        return "#" * self.level + " " + self.title + "\n\n" + self.text
